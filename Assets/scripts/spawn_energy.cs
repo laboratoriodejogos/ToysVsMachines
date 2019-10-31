99deb88f5438e551b07b21ef1cfae0d2bdf75796
IsaiasGameDev <54710832+Isaiasgamedev@users.noreply.github.com>
2019-10-31 09:14:26 -0300

    Add files via upload

---
+++ Assets/scripts/spawn_energy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_energy : MonoBehaviour
+
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawn", 10, 10);
+
+    }
+
+        private void spawn()
+        {
+            Instantiate(prefab, transform.position, Quaternion.identity);
+        }
+
+}
---
+++ Assets/scripts/velocidade_padrao.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class velocidade_padrao : MonoBehaviour
+{
+    public Vector2 velocidade;
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = velocidade;
+    }
+
+
+}
